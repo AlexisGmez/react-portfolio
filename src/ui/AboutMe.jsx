7cf817401a68f3fe4b1c0c7044db860f66b3267b
AlexisGmez <alexisgmez65@gmail.com>
2023-01-14 11:52:21 -0600

    adding changes

--- src/ui/AboutMe.jsx
+++ src/ui/AboutMe.jsx
@@ -4,9 +4,8 @@ const AboutMe = () => {
   return (
     <section className='aboutMe' id='aboutMe'>
         <h2>Acerca de mi</h2>
-        <p>Hola a cualquiera que este leyendo mi portfolio, me presento soy Alexis Medrano y soy ingeniero en sistemas, graduado del Insituto Tecnologico Superior de Uruapan en junio del año 2022.<br/> Desarrollador Web Full Stack Js dichos conocimientos fueron adquiridos en Academlo una escuela online especializada en desarrollo web. <br/> <b>Tecnologias: </b>Html, Css, Sass, Bootstrap, React, NodeJs, PostgreSql </p>
+        <p>Hola a cualquiera que este leyendo mi portfolio, me presento soy Alexis Medrano y soy ingeniero en sistemas.👓, graduado del Insituto Tecnologico Superior de Uruapan en junio del año 2022.📔<br/> Desarrollador Web Full Stack Js dichos conocimientos fueron adquiridos en Academlo una escuela online especializada en desarrollo web. 🧑🏻‍💻<br/> <b>Tecnologias: </b>Html, Css, Sass, Bootstrap, React. <br/>Estoy listo para afrontar los desafios de la industria dando soluciones con mis abilidades adquiridas. </p>
         <a href="./Medrano Gomez Alan Alexis.pdf" download> <i className='bx bx-down-arrow-alt'></i> Descargar CV </a>
-        
     </section>
   )
 }
--- src/ui/HeaderContent.jsx
+++ src/ui/HeaderContent.jsx
@@ -5,11 +5,11 @@ const HeaderContent = () => {
     <section className='header__content'>
       <div className="img__container">
         <img src={Me} alt="" />
-        <p className='p__fullStack'>Full Stack<br/>Developer</p>
+        <p className='p__fullStack'>Front-End<br/>Developer</p>
       </div>
       <div className="header__aboutMe">
         <h1>Soy <br/><span>Alexis Medrano</span></h1>
-        <h3>Desarrollador Full Stack Jr</h3>
+        <h3>Desarrollador Frontend Jr</h3>
         <div className="header__social">
             <a href="https://www.linkedin.com/in/alan-alexis-medrano-gomez-4431b4218/" target='_blank'><i className='bx bxl-linkedin'></i></a>
             <a href="https://www.instagram.com/alexiisgmez/" target='_blank'><i className='bx bxl-instagram-alt'></i></a>
